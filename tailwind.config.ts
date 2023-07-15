c7f43f2f4dbf831da143b505819d7e17a5f6047f
colmpat <cplang@dons.usfca.edu>
2023-07-15 16:43:46 -0700

    feat: add Topbar component

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import { type Config } from "tailwindcss";
+
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        canvas: '#fff',
+        canvasInset: '#f6f8fa',
+
+        fgDefault: '#24292F',
+        fgMuted: '#57606a',
+
+        borderDefault: "rgba(27,31,36,.15)",
+        primaryPurple: "#8250df",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
