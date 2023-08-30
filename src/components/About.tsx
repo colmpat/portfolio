013f39b95f486ac2dc353a400472683c9f868c09
colmpat <cplang@dons.usfca.edu>
2023-08-29 19:51:10 -0700

    fix: remove unescaped '

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,11 +5,11 @@ const About: React.FC = () => {
     <div id="home" className="w-full flex space-between border-b pb-12">
       {/* Text Intro */}
       <div className="mt-20 flex flex-col w-2/3 gap-2">
-        <h3 className="text-2xl font-bold text-fgMuted">Hi, I'm Colm Lang:</h3>
+        <h3 className="text-2xl font-bold text-fgMuted">Hi, I&apos;m Colm Lang:</h3>
         <h1 className="text-5xl font-extrabold">Software Engineer</h1>
         <h3 className="mt-1 text-3xl font-bold">and Computer Science 4+1 Student @ <a href="https://www.usfca.edu/" rel="noreferrer" target="_blank" className="text-primaryPurple hover:underline">USF</a></h3>
         <p className="mt-8 leading-10 text-xl text-fgMuted indent-4">
-          I'm a results-driven Software Engineer who excels in fast-paced environments, consistently delivering 
+          I&apos;m a results-driven Software Engineer who excels in fast-paced environments, consistently delivering 
           an exceptional rate of development to the teams and projects I work on. My passion centers around 
           projects that harness <b>big data</b>, <b> distributed systems</b>, and <b>data visualization</b> to 
           solve problems with <span className="italic text-primaryPurple">real human impact</span>. Excited to 
